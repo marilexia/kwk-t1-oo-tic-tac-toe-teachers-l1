94317e8860af5cfaa69a6d9cb8998ef44bc27d9a
Marilexia Faith Acelajado <faithacelajado@gmail.com>
2018-06-09 01:03:16 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -85,6 +85,7 @@ class TicTacToe
         if @board[win_combination[0]] == "X" && @board[win_combination[1]] == "X" && @board[win_combination[2]] == "X" || @board[win_combination[0]] == "O" && @board[win_combination[1]] == "O" && @board[win_combination[2]] == "O"
           return win_combination
         end
+      end
           return false
         end
       end
