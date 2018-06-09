8ee4abcf4b3f8d68242bd52da45b235a0822a195
Marilexia Faith Acelajado <faithacelajado@gmail.com>
2018-06-09 01:04:04 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -79,7 +79,7 @@ class TicTacToe
       turn_count % 2 == 0 ? "X" : "O"
     end
 
-    def won?(board)
+    def won?
 
       WIN_COMBINATIONS.each do |win_combination|
         if @board[win_combination[0]] == "X" && @board[win_combination[1]] == "X" && @board[win_combination[2]] == "X" || @board[win_combination[0]] == "O" && @board[win_combination[1]] == "O" && @board[win_combination[2]] == "O"
